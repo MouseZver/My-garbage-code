1f2bd6aed51f4bb18ca66e61b4a75082611d5def
MouseZver <mouse-zver@xaker.ru>
2017-08-09 19:02:42 +0300

    Release

--- upload_class/Upload.php
+++ upload_class/Upload.php
@@ -1,6 +1,16 @@
 <?php
 
-class GDImageUp
+error_reporting ( E_ALL );
+
+interface FilesUp
+{
+	public function SaveDirectory( string $DIRECTORY );	# Директория для сохранения файла
+	public function SaveFile(): bool;				# Функция валидации и сохранения файла
+	public function FileName(): string;				# Вывод имя файла с расширением
+	public function ErrorMessage(): array;			# Вывод ошибок
+}
+
+class GDImageUp implements FilesUp
 {
 	# Максимальный размер файла
 	const MAX_SIZE_IMG = ( 1024 * 1024 * 5 );
@@ -9,7 +19,7 @@ class GDImageUp
 	const MIME_TYPE = [ 'image/png', 'image/jpeg', 'image/gif' ];
 	
 	private 
-		$_mime_type = NULL,
+		$_mime_type = [],
 		$_max_size_img,
 		$_width = 200,
 		$_height = 200,
@@ -17,7 +27,7 @@ class GDImageUp
 		$_name,
 		$_E = [];
 	
-	public function __construct ( string $KEY, $SIZE = NULL )
+	public function __construct ( string $KEY, $SIZE = FALSE )
 	{
 		$this -> _key = $KEY;
 		$this -> _name = basename ( $_FILES[$KEY]['name'] );
@@ -36,13 +46,13 @@ class GDImageUp
 		
 		return $this;
 	}
-	public function ImageDirectory( string $DIR )
+	public function SaveDirectory( string $DIR )
 	{
 		$this -> _directory = "{$_SERVER['DOCUMENT_ROOT']}/{$DIR}";
 		
 		return $this;
 	}
-	public function ImageName( string $NAME )
+	public function SaveName( string $NAME )
 	{
 		$this -> _name = $NAME;
 		
@@ -52,19 +62,48 @@ class GDImageUp
 	{
 		return $this -> _E;
 	}
-	public function run(): bool
+	public function FileName(): string
 	{
+		return $this -> _name;
+	}
+	private function ErrorCode( int $A ): string
+	{
+		return [
+			0 => FALSE,
+			2 => FALSE,
+			UPLOAD_ERR_INI_SIZE => 'Размер принятого файла превысил максимально допустимый размер.',
+			UPLOAD_ERR_PARTIAL => 'Загружаемый файл был получен только частично.',
+			UPLOAD_ERR_NO_FILE => 'Файл не был загружен.',
+			UPLOAD_ERR_NO_TMP_DIR => 'Отсутствует временная папка.',
+			UPLOAD_ERR_CANT_WRITE => 'Не удалось записать файл на диск.',
+			UPLOAD_ERR_EXTENSION => 'PHP - расширение остановило загрузку файла'
+		][$A];
+	}
+	public function SaveFile(): bool
+	{
+		if ( !isset ( $_FILES[$this -> _key] ) )
+		{
+			$this -> _E['undefined'] = 'Undefined input name in:' . $this -> _key;
+			
+			return FALSE;
+		}		
+		if ( !empty ( $this -> ErrorCode( $_FILES[$this -> _key]['error'] ) ) )
+		{
+			$this -> _E['message'] = $this -> ErrorCode( $_FILES[$this -> _key]['error'] );
+			
+			return FALSE;
+		}
 		if ( !file_exists ( $this -> _directory ) )
 		{
-			$this -> _E[] = 'Invalid is directory';
+			$this -> _E['dir'] = 'Invalid is directory';
 		}
-		if ( count ( array_diff ( $A, self::MIME_TYPE ) ) > 0 )
+		if ( count ( array_diff ( $this -> _mime_type, self::MIME_TYPE ) ) > 0 )
 		{
-			$this -> _E[] = 'Denied access Mime type in: ' . implode ( ', ', $D );
+			$this -> _E['property_type'] = 'Denied access Mime type in: ' . implode ( ', ', $D );
 		}
-		elseif ( !in_array ( $_FILES[$this -> _key]['type'], ( $_MIME_TYPE ?? self::MIME_TYPE ) ) )
+		elseif ( !in_array ( $_FILES[$this -> _key]['type'], ( $this -> _mime_type ?: self::MIME_TYPE ) ) )
 		{
-			$this -> _E[] = 'Invalid is Mime type File';
+			$this -> _E['type'] = 'Invalid Mime type File';
 		}
 		else
 		{
@@ -86,7 +125,7 @@ class GDImageUp
 			
 			if ( $this -> _width < $SX || $this -> _height < $SY )
 			{
-				$this -> _E[] = 'Invalid image size width/height';
+				$this -> _E[] = 'Invalid image size';
 			}
 			if ( $this -> _max_size_img < $_FILES[$this -> _key]['size'] )
 			{
@@ -103,7 +142,7 @@ class GDImageUp
 		switch ( $_FILES[$this -> _key]['type'] )
 		{
 			case 'image/png':
-				$NAME = $this -> _name . '.PNG';
+				$NAME = $this -> _name . '.png';
 				ImagePNG ( $IMG, $this -> _directory . DIRECTORY_SEPARATOR . $NAME );
 			break;
 			case 'image/jpeg':
@@ -116,11 +155,24 @@ class GDImageUp
 			break;
 		}
 		
-		ImageDestroy ( $IMG, $IGD );
+		ImageDestroy ( $IMG );
 		
 		return $NAME;
 	}
 }
 
-$G = new GDImageUp( 'pictures', ( 1024 * 1024 * 2 ) );
-var_dump ( $G -> setMimeType( 'image/png' ) -> ImageSize( 200, 200 ) -> ImageDirectory( 'upload/' ) -> ImageName( 'lalka' ) -> run() );
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
+{
+	$G = new GDImageUp( 'pictures', ( 1024 * 1024 * 2 ) );
+	
+	if ( $G -> ImageSize( 2000, 2000 ) -> SaveDirectory( 'upload/' ) -> SaveName( 'lalka' ) -> SaveFile() === FALSE )
+	{
+		var_dump ( $G -> ErrorMessage() );
+	}
+	echo $G -> FileName();
+}
+?>
+<form enctype="multipart/form-data" method="post" action="/Upload.php">
+<input type="file" name="pictures">
+<input type="submit" value="ooooo">
+</form>
