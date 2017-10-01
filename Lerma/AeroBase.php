ecd455156d101d6af77b65b8841bdca3293e6895
Mouse <mouse-zver@xaker.ru>
2017-10-01 11:13:59 +0300

    Separator

--- Lerma/AeroBase.php
+++ Lerma/AeroBase.php
@@ -1,25 +1,7 @@
 <?php
 
-namespace Aero;
-
-class AeroBase
+class Aero
 {
-	protected static $NAMESPACESMAP = [];
-	
-	public static function autoload( string $C )
-	{
-		if ( isset ( self::$NAMESPACESMAP[$C] ) )	# Преобразование загруженной директории из имени пространства
-		{
-			$C = self::$NAMESPACESMAP[$C];
-		}
-		elseif ( file_exists ( __DIR__ . substr ( $C, 4 ) . '.php' ) )
-		{
-			$C = __DIR__ . substr ( $C, 4 );
-		}
-		else throw new \Exception( sprintf ( '~~ Invalid namespace %s', $C ) );
-		
-		include strtr ( $C, '\\', DIRECTORY_SEPARATOR ) . '.php';
-	}
 	public static function Separator( string $string )
 	{
 		return strtr ( $_SERVER['DOCUMENT_ROOT'] . $string, '/', DIRECTORY_SEPARATOR );
