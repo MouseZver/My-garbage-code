a9065434ff0321281c125058c1d51418905784e1
Mouse <mouse-zver@xaker.ru>
2017-10-31 22:04:20 +0300

    Interfase atributes

--- Lerma/Interfaces/Lerma/IDrivers.php
+++ Lerma/Interfaces/Lerma/IDrivers.php
@@ -7,7 +7,7 @@ interface LermaDrivers
 	public function query	( string $sql ): LermaDrivers;
 	public function prepare	( string $sql ): LermaDrivers;
 	public function execute	( array $arguments );
-	public function fetch	( int $fetch_style );
-	public function fetchAll( int $fetch_style );
+	public function fetch	( int $fetch_style, $fetch_argument );
+	public function fetchAll( int $fetch_style, $fetch_argument );
 	public function InsertId(): int;
 }
