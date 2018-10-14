235dcaf7f56cd0bd6caa5624a1286d3db62371fe
Mouse <mouse-zver@xaker.ru>
2018-10-14 12:04:07 +0300

    Update index.php

--- php.ru/67269/index.php
+++ php.ru/67269/index.php
@@ -2,21 +2,26 @@
 
 error_reporting ( E_ALL );
 
+# Название классов с их пространств имен
 const SPACES = [ 
 	webog\app\Hello::class, 
 	webog\Nub::class, 
 	Aero\Application\Purpure\InThisMoment\Lol::class 
 ];
 
+# Замена пути поиска файла
 const NAMESPACESMAP = [ 
 	Aero\Application\Purpure\InThisMoment\Lol::class => 'Lol' 
 ];
 
+# автоподключение файла по требованию в будущем
 spl_autoload_register ( function ( $a )
 {
 	include strtr ( ( NAMESPACESMAP[$a] ?? $a ), '\\', DIRECTORY_SEPARATOR ) . '.php';
 } );
 
+# выбираем рандомный класс из 6 строки
 $class = SPACES[array_rand ( SPACES )];
 
+# Юзаем
 echo new $class . $class;
